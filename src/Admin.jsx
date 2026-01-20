057ccc30caaf87ae9a68faf21fe0016c54dbbbff
sara-trueigtech <sakhan@trueigtech.com>
2026-01-20 18:10:32 +0530

    feat: creating react app + displaying data using jsx + creating and importing components

---
+++ src/Admin.jsx
@@ -0,0 +1,9 @@
+const Admin = () => {
+    return(
+        <>
+            <h1>Admin Panel</h1>
+        </>
+    )
+}
+
+export default Admin;
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import MyButton from "./MyButton";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const user = {
+    imgUrl: "https://i.imgur.com/yXOvdOS.jpg",
+    name: "sara",
+    imgSize: 90,
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="name">{user.name}</h1>
+      <MyButton />
+      <img
+        src={user.imgUrl}
+        alt={"photo of" + user.name}
+        style={{ width: user.imgSize, height: user.imgSize }}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
