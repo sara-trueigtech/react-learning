f4c8ae2d530d0b67bdd78058667ae821d5c0f52b
sara-trueigtech <sakhan@trueigtech.com>
2026-01-20 18:14:54 +0530

    feat: conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
+import Admin from "./Admin";
 import "./App.css";
+import Login from "./Login";
 import MyButton from "./MyButton";
 
 function App() {
-  const user = {
-    imgUrl: "https://i.imgur.com/yXOvdOS.jpg",
-    name: "sara",
-    imgSize: 90,
-  };
+  // const user = {
+  //   imgUrl: "https://i.imgur.com/yXOvdOS.jpg",
+  //   name: "sara",
+  //   imgSize: 90,
+  // };
+
+  const isLoggedin = true;
 
   return (
     <>
-      <h1 className="name">{user.name}</h1>
+      {/* <h1 className="name">{user.name}</h1>
       <MyButton />
       <img
         src={user.imgUrl}
         alt={"photo of" + user.name}
         style={{ width: user.imgSize, height: user.imgSize }}
-      />
+      /> */}
+
+      {
+        isLoggedin ? <Admin/> : <Login />
+      }
+
+      {/* {
+        isLoggedin && <Admin/>
+      } */}
     </>
   );
 }
