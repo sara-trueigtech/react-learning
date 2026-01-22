1d914ce674ee142013a8c2e4f1560827da6c08ae
sara-trueigtech <sakhan@trueigtech.com>
2026-01-22 17:18:19 +0530

    feat: Handling form submission errors

--- src/forms/Forms.jsx
+++ src/forms/Forms.jsx
@@ -1,51 +1,17 @@
-import React, {
-  useOptimistic,
-  useState,
-  startTransition
-} from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 const Forms = () => {
-  const [messages, setMessages] = useState([]);
-
-  async function sendMsg(text) {
-    await new Promise(res => setTimeout(res, 2000));
+  function search() {
+    throw new Error("search error");
   }
-
-  const [optMsg, addOptMsg] = useOptimistic(
-    messages,
-    (state, newMsg) => [...state, newMsg]
-  );
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-
-    const text = e.target.message.value;
-    e.target.reset();
-
-    // ✅ optimistic update MUST be in a transition
-    startTransition(() => {
-      addOptMsg({ text, sending: true });
-    });
-
-    await sendMsg(text);
-
-    setMessages(prev => [...prev, { text, sending: false }]);
-  }
-
   return (
     <>
-      <ul>
-        {optMsg.map((m, i) => (
-          <li key={i}>
-            {m.text} {m.sending && "sending"}
-          </li>
-        ))}
-      </ul>
-
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="message" />
-        <button>Send</button>
-      </form>
+      <ErrorBoundary fallback={<p>errorrr occurred</p>}>
+        <form action={search}>
+          <input type="text" name="message" />
+          <button type="submit">Send</button>
+        </form>
+      </ErrorBoundary>
     </>
   );
 };
