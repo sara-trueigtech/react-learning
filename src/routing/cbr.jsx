90f51ca0c202881b279cef9ac95ec2d9dee4fbcf
sara-trueigtech <sakhan@trueigtech.com>
2026-01-22 18:50:41 +0530

    feat: nested routes

--- src/routing/cbr.jsx
+++ src/routing/cbr.jsx
@@ -2,11 +2,18 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Services from "./Services";
+import Layout from "./Layout";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/about", element: <About /> },
-  { path: "/services", element: <Services /> },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "about", element: <About /> },
+      { path: "services", element: <Services /> },
+    ],
+  },
 ]);
 
 export default router;
